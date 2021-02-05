19691220b541cbc32d05e0ae3daaadc01528d477
peytonkemp <peyton.kemp@gmail.com>
2021-02-05 15:59:01 -0600

    parks detail button populates to the DOM

--- scripts/parks/Parks.js
+++ scripts/parks/Parks.js
@@ -1,9 +1,48 @@
+import { useParks } from "./ParkProvider.js"
+
 export const ParksHtml = (park) => {
     return `
         <section class="park">
-            <h3 class="attraction__name">${park.fullName}</h3>
-            <div class="attraction__">${park.states}</div>
-            <button id="attraction--${park.id}">${park.fullName} Details</button>
+            <h3 class="park__name">${park.fullName}</h3>
+            <div class="park__">${park.states}</div>
+            <button id="park--${park.id}">${park.fullName} Details</button>
         </section>
+    `
+}
+
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".parkDiv")
+
+eventHub.addEventListener("click", event => {
+    // console.log(event)
+    if(event.target.id.startsWith("park--")) {
+        const [prefix, parkId] = event.target.id.split("--") 
+    
+            const parksArray = useParks()
+            const selectedPark = parksArray.find((park) => park.id === parkId)
+            console.log(selectedPark)
+            contentTarget.innerHTML = `
+            ${detailsWindow(selectedPark)}
+            `
+            // detailsWindow(selectedpark)
+        // console.log(prefix, event)
+        const customEvent = new CustomEvent("ParkButtonClicked", {
+            detail: {
+                parkId: parseInt(parkId)
+            }
+        })
+        // console.log(customEvent)
+        eventHub.dispatchEvent(customEvent) 
+    }
+})
+
+export const detailsWindow = (park) => {
+    // console.log(park)
+    return `
+    <section class="windowContainerContent">
+    <h2>Park: ${park.fullName}</h2>
+    <p>Description: ${park.description}</p> 
+    <p>Activities: ${park.activities[1]}</p>
+    </section> 
     `
 }
