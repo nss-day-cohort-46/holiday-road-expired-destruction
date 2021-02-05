fecf9f15b69e140996b1c6f0a867c19816b200df
ericberringer <eric.berringer@gmail.com>
2021-02-05 15:19:41 -0600

    Parks Detail button now renders the rest of the info to the DOM.

--- scripts/attractions/Attractions.js
+++ scripts/attractions/Attractions.js
@@ -1,3 +1,5 @@
+import { useAttractions } from "./AttractionProvider.js"
+
 export const AttractionsHtml = (attraction) => {
     return `
         <section class="attraction">
@@ -6,5 +8,44 @@ export const AttractionsHtml = (attraction) => {
             <div class="attraction__">${attraction.city}</div>
             <button id="attraction--${attraction.id}">${attraction.name} Details</button>
         </section>
+    `
+}
+
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".attractionDiv")
+
+eventHub.addEventListener("click", event => {
+    // console.log(event)
+    if(event.target.id.startsWith("attraction--")) {
+        const [prefix, attractionId] = event.target.id.split("--") 
+    
+            const attractionsArray = useAttractions()
+            const selectedAttraction = attractionsArray.find((attraction) => attraction.id === parseInt(attractionId))
+            contentTarget.innerHTML = `
+            ${detailsWindow(selectedAttraction)}
+            `
+            // detailsWindow(selectedattraction)
+        // console.log(prefix, event)
+        const customEvent = new CustomEvent("attractionButtonClicked", {
+            detail: {
+                attractionId: parseInt(attractionId)
+            }
+        })
+        // console.log(customEvent)
+        eventHub.dispatchEvent(customEvent) 
+    }
+})
+
+const detailsWindow = (attraction) => {
+    console.log(attraction.ameneties)
+    return `
+    <section class="windowContainerContent">
+    <h2>Attraction: ${attraction.name}</h2>
+    <p>City: ${attraction.city}</p> 
+    <p>State: ${attraction.state}</p> 
+    <p>${attraction.description}</p> 
+    <h4>Ameneties:</h4>
+    <p>Restrooms: ${attraction.ameneties.restrooms}</p>
+    </section> 
     `
 }
