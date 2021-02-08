893ecf0faff23266d801958337e5e2213c2e17f6
dtracydepp <tracydepp82@gmail.com>
2021-02-08 11:04:45 -0600

    updated weather provider and api fetch for lon and lat. fetch working with numbers inputed

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -6,9 +6,9 @@ export const useWeather = () => {
     return weather.slice()
 }
 
-export const getWeather = () => {
+export const getWeather = (lat,lon) => {
 // only able to fetch one zip at a time but is in imperial units so it shows as degrees. Can also do a city but not sure how to pull all of them at once. or any locaiton we need??
-    return fetch (`https:api.openweathermap.org/data/2.5/weather?zip=37013,us&APPID=${settings.weatherKey}&units=imperial`)
+    return fetch (`http://api.openweathermap.org/data/2.5/forecast?lat=${35}&lon=${139}&appid=${settings.weatherKey}&units=imperial`)
     .then (response => response.json())
         .then(parsedweather =>{
             console.log("weather",parsedweather)
