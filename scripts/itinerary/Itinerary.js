cb3640329ade45d6240cc1e824c92ad0d85835f0
Erica Thompson <younique98@gmail.com>
2021-02-08 09:41:37 -0800

    added button disabled if statement

---
+++ scripts/itinerary/Itinerary.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+if( $('.attractionContainer || .eateryContainer || .parkContainer').is(':empty')) {
+    
+
+    document.getElementById("saveButton").disabled = true;
+} else {
+    document.getElementById("saveButton").disabled = false;
+}
+
+
+
+
+
+
+
+
+
+
+// const enbabledButton = document.getElementById("saveButton").disabled;
+
+// function disableBtn() {
+//     document.getElementById("saveButton").disabled = true;
+// }
+
+// function enableBtn() {
+//     document.getElementById("saveButton").disabled = false;
+// }
